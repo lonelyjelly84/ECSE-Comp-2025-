3db073391eee669510f6b91c79015492eeed0561
Josh <eejl609@aucklanduni.ac.nz>
2025-09-09 20:24:23 +1200

    LCD code to display a string

--- Firmware/I2C_LCD_DISPLAY.py
+++ Firmware/I2C_LCD_DISPLAY.py
@@ -34,3 +34,20 @@ bus.write_byte_data(
 )  # 101000 Data length, number of lines, font size
 bus.write_byte_data(module_address, 0x00, 0x01)  # 000001 Clear display
 time.sleep(0.0005)  # Delay to allow commands to process
+
+
+def lcd_display_string(string, line):
+    """Send string to display"""
+    if line == 1:
+        pos = 0x80
+    elif line == 2:
+        pos = 0xC0
+    elif line == 3:
+        pos = 0x94
+    elif line == 4:
+        pos = 0xD4
+
+    bus.write_byte_data(module_address, 0x00, pos)
+
+    for char in string:
+        bus.write_byte_data(module_address, 0x40, ord(char))
